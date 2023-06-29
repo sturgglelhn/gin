88f5c86a49f394cba49382c223f2ee48323929f0
Gandolf <m15572018661@163.com>
2023-06-29 13:54:13 +0800

    6-29 第一次推送

---
+++ gin-basic/gin_route/02_route_form.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+/*表单参数*/
+
+func main() {
+	r := gin.Default()
+	r.POST("/form", func(c *gin.Context) {
+		types := c.DefaultPostForm("type", "post")
+		username := c.PostForm("username")
+		password := c.PostForm("userpassword")
+		c.String(http.StatusOK, fmt.Sprintf("username:%s,password:%s,type:%s", username, password, types))
+	})
+
+	// 默认是8080
+	r.Run()
+}
